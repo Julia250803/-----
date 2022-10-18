751917a4336cf8bae9edc7ea6364cec4b83703b5
LAPTOP-J1OVBUHG\79133 <>
2022-10-18 21:29:17 +0700

    ddd

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -6,7 +6,9 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, z;
+            
+            
         }
     }
 }
