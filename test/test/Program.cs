1a65e343a332d5144b8ec93b49c414650d005f3a
LAPTOP-J1OVBUHG\79133 <>
2022-10-19 19:39:51 +0700

    24

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -6,7 +6,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            double a, b, z;
+            
 
             Console.WriteLine("Введите второе число");
             b = Convert.ToDouble(Console.ReadLine());
