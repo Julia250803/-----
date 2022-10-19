80669ed6166a4deff984b57e8bc4a6087bb4d7c0
LAPTOP-J1OVBUHG\79133 <>
2022-10-19 19:51:07 +0700

    233

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -6,7 +6,11 @@ namespace test
     {
         static void Main(string[] args)
         {
-            double a, b, z;
+            
+
+
+
+
             Console.WriteLine("Введите первое число");
             a = Convert.ToDouble(Console.ReadLine());
             
