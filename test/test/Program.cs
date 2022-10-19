e459ce9ac0b446bb841a18f60724bbdf851010bb
LAPTOP-J1OVBUHG\79133 <>
2022-10-19 19:55:42 +0700

    239

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -6,26 +6,9 @@ namespace test
     {
         static void Main(string[] args)
         {
-            double a, b, z;
-            z = 0;
-            a = 0;
-            b = 0;
-            switch (z)
-            {
-                case 1:
-                  
-                case 2:
-                    Console.WriteLine(a - b);
-                    break;
-                case 3:
-               
-                case 4:
-                             
-                default:
-                    Console.WriteLine("Такого варианта нет");
-                    break;
-
-            }
+            
+             Console.WriteLine(a - b);
+              
         }
     }
 }
