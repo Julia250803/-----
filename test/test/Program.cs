a8b266d14f6acd605f64739e56cd79013c07f01c
LAPTOP-J1OVBUHG\79133 <>
2022-10-19 19:52:50 +0700

    235

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -8,6 +8,11 @@ namespace test
         {
             
 
+
+
+
+
+
             Console.WriteLine("Выберите число: 1. Выполнить сложение. 2. Выполнить вычитание. 3. Выполнить умножение. 4. Выполнить деление");
             z = Convert.ToInt32(Console.ReadLine());
         }
