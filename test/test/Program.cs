68322fb30547f641c5b2ca155dc3600e00884dce
LAPTOP-J1OVBUHG\79133 <>
2022-10-19 12:02:23 +0700

    11111

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -8,9 +8,8 @@ namespace test
         {
             double a, b, z;
 
-            Console.WriteLine("Введите второе число");
-            b = Convert.ToDouble(Console.ReadLine());
-
+            Console.WriteLine("Выберите число: 1. Выполнить сложение. 2. Выполнить вычитание. 3. Выполнить умножение. 4. Выполнить деление");
+            z = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
